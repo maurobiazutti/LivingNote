870a97617f8c236579640a1b420caa4e576f8881
Mauro <mauro_biazutti@hotmail.com>
2026-07-17 15:45:17 -0300

    feat: criando class Certification

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -7,6 +7,7 @@ class Profile < ApplicationRecord
   has_many :educations, dependent: :destroy
   has_many :skills, dependent: :destroy
   has_many :languages, dependent: :destroy
+  has_many :certifications, dependent: :destroy
    
   validates :full_name, presence: { message: "deve ser preenchido obrigatoriamente" }
   validates :cpf,
