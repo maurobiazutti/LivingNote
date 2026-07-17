77951753a6ae1b0c225ca05fb558dca06ab515f2
Mauro <mauro_biazutti@hotmail.com>
2026-07-17 19:57:13 -0300

    feat: ajuste no CreateProfile e execução das migrates

--- db/migrate/20260717174504_create_profiles.rb
+++ db/migrate/20260717174504_create_profiles.rb
@@ -6,9 +6,15 @@ class CreateProfiles < ActiveRecord::Migration[8.1]
       t.string :platform_name
       t.string :phone
       t.text :bio
-      t.references :user, null: false, foreign_key: true, type: :uuid
+      # t.references :user, null: false, foreign_key: true, type: :uuid
 
       t.timestamps
     end
+    # 2. Adiciona a coluna bigint depois que a tabela já nasceu isolada
+    add_column :profiles, :user_id, :bigint, null: false
+    
+    # 3. Cria o índice e a chave estrangeira manualmente
+    add_index :profiles, :user_id
+    add_foreign_key :profiles, :users, column: :user_id
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,105 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_07_16_231854) do
+ActiveRecord::Schema[8.1].define(version: 2026_07_17_200450) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pgcrypto"
+
+  create_table "addresses", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "city"
+    t.string "country"
+    t.datetime "created_at", null: false
+    t.string "district"
+    t.string "number"
+    t.uuid "profile_id", null: false
+    t.string "state"
+    t.string "street"
+    t.datetime "updated_at", null: false
+    t.string "zip_code"
+    t.index ["profile_id"], name: "index_addresses_on_profile_id"
+  end
+
+  create_table "certifications", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.date "issue_date"
+    t.string "issuer"
+    t.string "name"
+    t.uuid "profile_id", null: false
+    t.datetime "updated_at", null: false
+    t.index ["profile_id"], name: "index_certifications_on_profile_id"
+  end
+
+  create_table "educations", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "course"
+    t.datetime "created_at", null: false
+    t.string "degree"
+    t.date "end_date"
+    t.string "institution"
+    t.uuid "profile_id", null: false
+    t.date "start_date"
+    t.datetime "updated_at", null: false
+    t.index ["profile_id"], name: "index_educations_on_profile_id"
+  end
+
+  create_table "experiences", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "company"
+    t.datetime "created_at", null: false
+    t.text "description"
+    t.date "end_date"
+    t.string "position"
+    t.uuid "profile_id", null: false
+    t.date "start_date"
+    t.datetime "updated_at", null: false
+    t.index ["profile_id"], name: "index_experiences_on_profile_id"
+  end
+
+  create_table "languages", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "level"
+    t.string "name"
+    t.uuid "profile_id", null: false
+    t.datetime "updated_at", null: false
+    t.index ["profile_id"], name: "index_languages_on_profile_id"
+  end
+
+  create_table "profiles", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.text "bio"
+    t.string "cpf"
+    t.datetime "created_at", null: false
+    t.string "full_name"
+    t.string "phone"
+    t.string "platform_name"
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
+  create_table "resumes", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.text "objective"
+    t.uuid "profile_id", null: false
+    t.string "title"
+    t.datetime "updated_at", null: false
+    t.index ["profile_id"], name: "index_resumes_on_profile_id"
+  end
+
+  create_table "skills", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "level"
+    t.string "name"
+    t.uuid "profile_id", null: false
+    t.datetime "updated_at", null: false
+    t.index ["profile_id"], name: "index_skills_on_profile_id"
+  end
+
+  create_table "social_links", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "platform"
+    t.uuid "profile_id", null: false
+    t.datetime "updated_at", null: false
+    t.index ["profile_id"], name: "index_social_links_on_profile_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -26,4 +121,14 @@ ActiveRecord::Schema[8.1].define(version: 2026_07_16_231854) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "addresses", "profiles"
+  add_foreign_key "certifications", "profiles"
+  add_foreign_key "educations", "profiles"
+  add_foreign_key "experiences", "profiles"
+  add_foreign_key "languages", "profiles"
+  add_foreign_key "profiles", "users"
+  add_foreign_key "resumes", "profiles"
+  add_foreign_key "skills", "profiles"
+  add_foreign_key "social_links", "profiles"
 end
